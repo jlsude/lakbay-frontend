fc120065464a98439e111a6df739fb2e3221d6ec
hawkdawg <113959779+jlsude@users.noreply.github.com>
2023-04-24 22:12:42 +0800

    4-24 monday night

---
+++ src/components/landingpage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        
+        <div class="logo-container">
+        <img class="logo" src="../assets/LakbayLogo.png" alt="Lakbay Logo">
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    @media (max-width: 500px) {
+        
+        .logo-container {
+            margin-top: 10vh;
+            max-width: 100%;
+            text-align: center;
+        }
+        .logo {
+            width: 50%;
+            margin-bottom: 30px;
+        }
+        .login-container {
+            /* border: 1px dashed gray; */
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+        }
+    }
+</style>
