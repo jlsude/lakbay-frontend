1cffbff46e1a7ee6b13c5862957b8b9409e6ea06
hawkdawg <113959779+jlsude@users.noreply.github.com>
2023-05-01 00:09:13 +0800

    May 1 Commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,53 @@
 <template>
-	<div style = "display: flex; flex-direction: column;">
-		<header class = "home-header">
-			<div class="logo-container">
-				<img class="logo" src="../assets/LakbayLogo.png" alt="Lakbay Logo">
-				<img class = "menu" src = "../assets/hamburgerMenu.png" alt = "Menu burger">
+	<div style = "display: flex; flex-direction: column; justify-content: center;">
+		<header class="home-header">
+			<div class="sidebar-container">
+				<div class="logo-container">
+					<img class="logo" src="../assets/LakbayLogo.png" alt="Lakbay Logo">
+					<img class="menu" src="../assets/hamburgerMenu.png" v-on:click="showingSidebar">
+				</div>
+				<div class="sidebar-wrapper" :class="{ active: showSidebar }">
+					<div class="sidebar-content">
+						<Sidebar v-if="showSidebar" />
+					</div>
+				</div>
+				<div class="overlay" v-if="showSidebar" v-on:click="showingSidebar"></div>
+				<img class="menuOverlay" v-if="showSidebar" src="../assets/hamburgerMenu.png" v-on:click="showingSidebar">
 			</div>
-			
 		</header>
 		<div class = "qrdiv">
+			
 			<button class = "qrbutton" v-on:click = "QRscanner">QR-Scanner</button>
 			<button class = "beginLakbay" v-on:click = "beginLakbay" >Begin your Lakbay</button>
 			<h1 class = "recentLakbay">Your Recent Lakbay</h1>
-			<button class = "LakbayRecent" >{{ LakbayRecent}}</button>
-			<button class = "LakbayRecent" >{{ LakbayRecent}}</button>
-			
+
+			<!-- To be concised -->
+			<div v-for = "userhistory in userhistory" :key = "userhistory.userhistory_id">
+				<button class = "LakbayRecent" v-on:click = "reDirectLakbay(userhistory)" v-if = "!showRecentLakbay">{{ userhistory.landmark_name }}</button>
+			</div>
+			<div v-for="i in 2" :key="i">
+				<button class = "LakbayRecentDataInsuficient" v-if = "showRecentLakbay">Data insuficient</button>
+			</div>
+			<!-- -------- -->
+			<h1 class = "LakbayBucketListHeader">Lakbay Bucket List</h1>
+			<div class = "footer-container">
+				<div class = "footer-section" v-for="i in 3" :key="i">
+				<button class = "BucketListButton" v-on:click = "reDirectBucketList()" v-if = "!showBucketLists">Challenge</button>
+				<button class = "BucketListButtonFalse" v-if = "showBucketLists">Data insuficient</button>
+				</div>
+			</div>
 		</div>
-		<footer class = "home-footer">
-			<button class = "logoutbutton" v-on:click = "logout">Logout</button>
-		</footer>
+		
 	</div>
 </template>
 
 <script>
-import Cookies from 'js-cookie'
-import axios from 'axios'
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import Sidebar from './Sidebar.vue';
 
 	export default {
+  		components: { Sidebar },
 		name: 'Home',
 		data(){
 			return{
@@ -36,14 +58,24 @@ import axios from 'axios'
 				user_lastname: "",
 			},
 			userToken: "",
-			LakbayRecent: "Casa Manila",
+			users: [],
+			userhistory: [],
+			BucketLists: [],
+
+			showSidebar: false,
+			showRecentLakbay: false,
+			showBucketLists: false,
+			
+
 
 			}
 		},
+
 		mounted(){
+			//Authorization
 			let userToken = Cookies.get('auth_token');
 			if (userToken) {
-				axios.get(`http://192.168.1.12:7000/home/u/userprofile`, {
+				axios.get(`http://localhost:7000/home/u/userprofile`, {
 				headers: {
 					Authorization: `Bearer ${userToken}`
 				}
@@ -62,28 +94,57 @@ import axios from 'axios'
 					console.log(error);
 				}
 				});
-			} else {
-				
+			} 
+			else {
 				console.log('clearing cookie')
                 Cookies.remove('auth_token'); 
                 this.authToken = ''
 				this.$router.push({name: 'Login'});
-			}
+			};
+
+			
+			//Recent Lakbay - focus on recent lakbay
+			axios.get(`http://192.168.1.21:7000/home/u/userhistory`, {
+				headers: { Authorization: `Bearer ${userToken}`}})
+			.then((response) => {
+				this.userhistory = response.data.slice(-2);
+				console.log('Recent Lakbay data', this.userhistory);
+
+				if (this.userhistory.length < 2) {
+					this.showRecentLakbay = !this.showRecentLakbay;
+					console.log('Recent Lakbay data insuficient', this.userhistory);
+					
+				} else {
+					
+      				console.log("Recent Lakbay");
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				this.error = 'Failed to fetch Landmarks.';
+			});
+
+
+		
 		},
+
 		methods: {
-			logout(){
-				console.log('deleting cookie')
-                Cookies.remove('auth_token'); 
-                this.authToken = ''
-				this.$router.push({name: 'Login'});
-			},
+			
 			QRscanner(){
-				
 				console.log('Routing to QR Scanner')
 				this.$router.push({name: 'QrScanner'});
+			},
+			showingSidebar(){
+				this.showSidebar = !this.showSidebar;
 				
-				
-				
+			},
+			reDirectLakbay(userhistory){
+				console.log(`Redirecting to ${userhistory.landmark_name}`)
+				console.log(`Redirecting to ${userhistory.landmark_id}`)
+				this.$router.push({ name: 'LakbayContent', params: {landmark_id: userhistory.landmark_id} });
+			},
+			reDirectBucketList(){
+
 			}
 		}
 	}
@@ -100,10 +161,10 @@ import axios from 'axios'
 		}
 		.home-header {
 			/* border: 1px dashed rgb(148, 19, 19); */
-			margin-top: -5vh;
+
 			margin-left: 1.5vw;
 			margin-right: 1.5vw;
-			}
+		}
 		.logo{
 			display: inline-block;
 			text-align: left;
@@ -115,6 +176,49 @@ import axios from 'axios'
 			float: right;
 			width: 10vw;
 			height: auto;
+			cursor: pointer;
+		}
+		.overlay {
+			position: fixed;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			width: 100%;
+			background-color: rgba(0, 0, 0, 0.5);
+			opacity: 0.5;
+			z-index: 9999;
+			transition: opacity 5s;
+			
+		}
+
+		.sidebar-wrapper {
+			position: fixed;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			width: 50%;
+			background-color: #fff;
+			z-index: 10000;
+			transform: translateX(100%);
+			transition: transform 0.3s ease-out;
+		}
+
+		.sidebar-wrapper.active {
+			transform: translateX(0%);
+		}
+		.menuOverlay{
+			position: fixed;
+			height: auto;
+			margin-top: -5.15vh;
+			margin-left: 72vw;
+			z-index: 10001;
+			width: 10vw;
+			cursor: pointer;
+
+			z-index: 10000;
+			transform: translateX(100%);
+			transition: transform 0.3s ease-out;
 		}
 		.qrdiv{
 			display: flex;
@@ -158,16 +262,15 @@ import axios from 'axios'
 			color: #000000;
 		}
 		.recentLakbay{
-			border: 1px dashed gray;
 			font-size: 2.5vh; 
 			font-weight: 400;
 			text-align: left; 
-			margin-left: 5vw; 
-			margin-right: 5vw;
+			margin-left: 8vw; 
+			margin-right: 8vw;
 			margin-top: 3vh;
 			margin-bottom: 0.2vh
 		}
-		.LakbayRecent{
+		.LakbayRecent {
 			border: none;
 			align-self: center;
 			width: 80vw;
@@ -180,29 +283,96 @@ import axios from 'axios'
 			font-family: 'Inter';
 			font-style: normal;
 			font-weight: 400;
-			font-size: 1.5em;
+			font-size: calc(0.5em + 3vw);
 			line-height: 1.5;
-			color: #000000
+			word-wrap: break-word;
+			text-overflow: ellipsis;
 		}
-		.logoutbutton{
-				border: none;
-				margin: 50px;
-				align-self: center;
-				width: 25%;
-				height: 40px;
-				border-radius: 10px;
-				background-color: #c4c4c4;
+		.LakbayRecentDataInsuficient{
+			border: none;
+			align-self: center;
+			width: 80vw;
+			height: 6vh;
+			border-radius: 1.4vh;
+			margin-top: 1vh;
+			background-color: #c4c4c4;
 
-				text-align: center;
-				font-family: 'Inter';
-				font-style: normal;
-				font-weight: 400;
-				font-size: 15px;
-				line-height: 1.5;
-				color: #000000;
-			}
+			opacity: 0.4;
+			text-align: center;
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 400;
+			font-size: calc(0.5em + 3vw);
+			line-height: 1.5;
+			word-wrap: break-word;
+			overflow-wrap: break-word;
+		}
+		.LakbayBucketListHeader{
+			font-size: 2.5vh; 
+			font-weight: 400;
+			text-align: center; 
+			margin-left: 5vw; 
+			margin-right: 5vw;
+			margin-top: 3vh;
+			margin-bottom: 0.2vh
+		}
+		.footer-container{
+			overflow-x: scroll;
+			scroll-snap-type: x mandatory;
+			display: flex;
+		}
+		.footer-section{
+			scroll-snap-align: start;
+			scroll-snap-stop: always;
+			padding-bottom: 10px;
+		}
+		.BucketListButton{
+			margin-left: 7.5vw;
+			margin-right: 8vw;
+			border: none;
+			align-self: center;
+			width: 80vw;
+			height: 6vh;
+			border-radius: 1.4vh;
+			margin-top: 1vh;
+			background-color: #c4c4c4;
+
+			text-align: center;
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 400;
+			font-size: calc(0.5em + 3vw);
+			line-height: 1.5;
+			word-wrap: break-word;
+			overflow-wrap: break-word;
+		}
+		.BucketListButtonFalse{
+			margin-left: 7.5vw;
+			margin-right: 8vw;
+			border: none;
+			align-self: center;
+			width: 80vw;
+			height: 6vh;
+			border-radius: 1.4vh;
+			margin-top: 1vh;
+			background-color: #c4c4c4;
+
+			opacity: 0.4;
+			text-align: center;
+			font-family: 'Inter';
+			font-style: normal;
+			font-weight: 400;
+			font-size: calc(0.5em + 3vw);
+			line-height: 1.5;
+			word-wrap: break-word;
+			overflow-wrap: break-word;
+		}
+
+
+
+
 		.beginLakbay:active, .logoutbutton:active,
-		.LakbayRecent:active {
+		.LakbayRecent:active, .BucketListButton:active {
 				background-color: #3C3C3C;
 			}
 	}
---
+++ src/components/LakbayContent.vue
@@ -0,0 +1,375 @@
+<template> 
+	<div style = "display: flex; flex-direction: column; justify-content: center;">
+		<div class = "header-card">
+			<header style = "margin-inline: 2.5vw; display: flex; flex-direction: column; justify-content: center; ">
+				<div class="Content-header" >
+					<div class="sidebar-container">
+						<div class="logo-container">
+							<img class="logo" src="../assets/LakbayLogo.png" alt="Lakbay Logo">
+							<img class="menu" src="../assets/hamburgerMenu.png" v-on:click="showingSidebar">
+						</div>
+						<div class="sidebar-wrapper" :class="{ active: showSidebar }">
+							<div class="sidebar-content">
+								<SidebarToHome v-if="showSidebar" />
+							</div>
+						</div>
+						<div class="overlay" v-if="showSidebar" v-on:click="showSidebar = false"></div>
+						<img class="menuOverlay" v-if="showSidebar" src="../assets/hamburgerMenu.png" v-on:click="showingSidebar">
+					</div>
+				</div>
+				<div>
+					<img class = "scanbutton" src="../assets/ScanButton.png" v-on:click="reDirectScan" alt= "ScanButton">
+					<h1 class = "LandmarkNameHeader">{{ landmarkinfos.landmark_name }}</h1>
+				</div>
+			</header>
+
+
+		</div>
+		<!-- ------------------------------------------------------------------------------------------ -->
+		<main class = "ContentMain">
+				<!-- Lakbay Image -->
+				<div class = "body">
+					<div class = "slider-container">
+						<div class = "slide" v-for="(image, index) in contentimagedata" :key="index" >
+							<img :src="image.url" alt = "">
+						</div>
+					</div>
+				</div>
+				<!-- Lakbay Infos -->
+				<h1 class = "LandmarkName"> {{ landmarkinfos.landmark_name }}</h1>
+				<h2 class = "LandmarkAddress">{{ landmarkinfos.landmark_address }}</h2>
+				<h3 class = "LandmarkRateVisits">
+					<p>5.0/5.0</p>
+					<p>{{ landmarkinfos.landmark_visits }} Have Visited</p>
+				</h3>
+
+				<p class = "LandmarkPara">{{ this.paragraphOne }}</p>
+
+				<div class = "body">
+					<div class = "slider-container">
+						<div class = "slide" v-for="(image, index) in 1" :key="index" >
+							<img :src="this.randomImage1" alt = "">
+							
+						</div>
+						
+					</div>
+				</div>
+				<p class = "caption">{{ this.randomImage1Cap }}</p>
+
+				<p class = "LandmarkPara">{{ this.paragraphTwo }}</p>
+
+				<div class = "body">
+					<div class = "slider-container">
+						<div class = "slide" v-for="(image, index) in 1" :key="index" >
+							<img :src="this.randomImage2" alt = "">
+						</div>
+					</div>
+				</div>
+				<p class = "caption">{{ this.randomImage1Cap }}</p>
+				<p class = "LandmarkPara">{{ this.paragraphThree }}</p>
+
+		</main>
+		<!-- ------------------------------------------------------------------------------------------ -->
+		
+	</div>
+</template>
+
+<script>
+import SidebarToHome from './SidebarToHome.vue';
+import axios from 'axios'
+
+
+export default {
+	name: 'LakbayContent',
+	components: {SidebarToHome},
+		data(){
+		return{
+			landmark_id: '',
+			contentimagedata: [],
+
+			landmarkinfos: {
+				landmark_name: '',
+				landmark_city: '',
+				landmark_address: '',
+				landmark_region: '',
+				landmark_coordinates: '',
+				landmark_visits: '',
+				landmark_typecontent: '',
+			},
+
+			landmarkparagraphs: [],
+			paragraphOne: '',
+			paragraphTwo: '',
+			paragraphThree: '',
+			showSidebar: false,
+			randomImage1: '',
+			randomImage1Cap: '',
+			randomImage2: '',
+			randomImage2Cap: '',
+        	
+		}
+		},
+	mounted() {
+		//Authorization
+		let userToken = Cookies.get('auth_token');
+			if (userToken) {
+				axios.get(`http://localhost:7000/home/u/userprofile`, {
+				headers: {
+					Authorization: `Bearer ${userToken}`
+				}
+				})
+				.then((response) => {
+				this.userProfile = response.data[0];
+				})
+				.catch(error => {
+				if (error.response && error.response.status === 401) {
+					// Token has expired
+					alert('Your session has expired. Please login again.');
+					console.log('Session expired, signing out')
+					Cookies.remove('auth_token'); 
+					this.$router.push({name: 'Login'});
+				} else {
+					console.log(error);
+				}
+				});
+			} 
+			else {
+				console.log('clearing cookie')
+                Cookies.remove('auth_token'); 
+                this.authToken = ''
+				this.$router.push({name: 'Login'});
+			};
+
+
+
+		this.landmark_id = this.$route.params.landmark_id;
+		console.log(`Landmark ID: ${this.landmark_id}`);
+		// fetch landmark_id and display it in the component
+
+		// Fetch landmark images
+		axios.get(`http://192.168.1.21:7000/manage/locations/${this.landmark_id}/images`)
+        .then((response) => {
+			this.contentimagedata = response.data
+			console.log(this.contentimagedata)
+			//----- Image Randomizer
+			let x1, x2, x3 = 0;
+			do {
+				x1 = Math.floor(Math.random() * this.contentimagedata.length);
+				x2 = Math.floor(Math.random() * this.contentimagedata.length);
+			} while (x1 === x2 || x1 === x3 || x2 === x3);
+
+				this.randomImage1 = this.contentimagedata[x1].url;
+				this.randomImage1Cap = this.contentimagedata[x1].caption;
+				this.randomImage2 = this.contentimagedata[x2].url;
+				this.randomImage2Cap = this.contentimagedata[x1].caption;
+			//------
+
+        }).catch((error) => {
+				console.error(error);
+				this.error = 'Failed to fetch landmark images.';
+		});
+
+		// Fetch landmark info NOT paragraphs
+		axios.post(`http://192.168.1.21:7000/LakbayScan/u/fetching`, {landmarkid: this.landmark_id})
+		.then((response) => {
+			this.landmarkinfos = response.data[0]
+			console.log('fetch landmark info')
+			if (this.landmarkinfos) {
+				console.log("Test", this.landmarkinfos.landmark_name); // problem is here
+			}
+			console.log(response.data)
+
+		}).catch((error) => {
+				console.error(error);
+				this.error = 'Failed to fetch landmark info.';
+		});
+
+		// Fetch landmark paragraphs
+		axios.post(`http://192.168.1.21:7000/LakbayScan/u/fetching/paragraphs`, {landmarkid: this.landmark_id})
+		.then((response) => {
+			this.landmarkparagraphs = response.data
+			console.log(' fetch landmark paragraphs')
+			console.log("Paragraph 1", this.landmarkparagraphs[0].info_paragraph)
+			this.paragraphOne = this.landmarkparagraphs[0].info_paragraph
+			console.log("Paragraph 2", this.landmarkparagraphs[1].info_paragraph)
+			this.paragraphTwo = this.landmarkparagraphs[1].info_paragraph
+			console.log("Paragraph 3", this.landmarkparagraphs[2].info_paragraph)
+			this.paragraphThree = this.landmarkparagraphs[2].info_paragraph
+			console.log(response.data)
+		}).catch((error) => {
+				console.error(error);
+				this.error = 'Failed to fetch landmark paragraphs.';
+		});
+
+
+		
+	},
+	methods: {
+		showingSidebar(){
+				this.showSidebar = !this.showSidebar;
+				console.log("Closing")
+			},
+		reDirectScan(){
+			console.log('Routing to QR Scanner')
+			this.$router.push({name: 'QrScanner'});
+		}
+	}
+	}
+</script>
+
+<style scoped>
+	@media(max-width: 500px){
+		.header-card{
+			background: #ffffff;
+			margin-top: -1vh;
+			width: 105%;
+			align-self: center;
+			border-bottom-left-radius: 9vw;
+			border-bottom-right-radius: 9vw;
+			box-shadow: 3px 17px 58px -10px rgba(0,0,0,0.4);
+			-webkit-box-shadow: 3px 17px 58px -10px rgba(0,0,0,0.4);
+			-moz-box-shadow: 3px 17px 58px -10px rgba(0,0,0,0.4);
+
+		}
+		.Content-header{
+			margin-top: 1vh;
+			margin-left: 1.5vw;
+			margin-right: 1.5vw;
+			
+		}
+		.logo{
+			display: inline-block;
+			text-align: left;
+			width: 40vw;
+			height: auto;
+		}
+		.menu{
+			margin-top: 1vh;
+			float: right;
+			width: 10vw;
+			height: auto;
+			cursor: pointer;
+		}
+		.scanbutton{
+			margin: 2vh;
+			float: left;
+			width: 10vw;
+			height: auto;
+			cursor: pointer;
+		}
+		.LandmarkNameHeader{
+			margin-top: 1vh;
+			margin-left: 18vw;
+			margin-right: 5vw;
+			padding-top: 1vh;
+			padding-bottom: 1vh;
+			font-size: 6.5vw;
+			border-bottom: 1px solid black;
+			
+		}
+		.overlay {
+			position: fixed;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			width: 100%;
+			background-color: rgba(0, 0, 0, 0.5);
+			opacity: 0.5;
+			z-index: 9999;
+			transition: opacity 5s;
+			
+		}
+
+		.sidebar-wrapper {
+			position: fixed;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			width: 50%;
+			background-color: #fff;
+			z-index: 10000;
+			transform: translateX(100%);
+			transition: transform 0.3s ease-out;
+		}
+
+		.sidebar-wrapper.active {
+			transform: translateX(0%);
+		}
+		.menuOverlay{
+			position: fixed;
+			height: auto;
+			margin-top: -5.15vh;
+			margin-left: 72vw;
+			z-index: 10001;
+			width: 10vw;
+			cursor: pointer;
+
+			z-index: 10000;
+			transform: translateX(100%);
+			transition: transform 0.3s ease-out;
+		}
+		.ContentMain{
+			margin-inline: 4vw;
+			
+		}
+		.body{
+			margin-top: 3vh;
+			display: grid;
+			place-items: center;
+		}
+		.slider-container{
+			height: 30vh;
+			width: 40vh;
+			display: flex;
+			overflow: auto;
+			scroll-snap-type: x mandatory;
+		}
+		.slider-container .slide{
+			scroll-snap-align: end;
+			padding-bottom: 25px;
+		}
+		.slider-container img{
+			width: 40vh;
+			height: 100%;
+			object-fit: cover;
+		}
+
+		.LandmarkName{
+			font-size: 6.5vw;
+			margin-top: 0.5vh;
+			margin-bottom: 0.5vh;
+
+		}
+		.LandmarkAddress{
+			font-size: 4.5vw;
+			font-weight: 600;
+			margin-top: 0.5vh;
+			margin-bottom: 0.5vh;
+		}
+		.LandmarkRateVisits{
+			display: flex; 
+			justify-content: space-between;
+			margin-top: -1vh;
+			font-size: 4.5vw;
+			font-weight: 500;
+			color: rgb(30, 30, 30);
+		}
+		.LandmarkPara{
+			margin-bottom: 5vh;
+			text-align: justify;
+		}
+		.caption{
+			margin-top: -2vh;
+			margin-left: 1.25vw;
+			font-size: smaller;
+			font-style: italic;
+			
+			
+		}
+
+	}
+</style>
+
+
+
